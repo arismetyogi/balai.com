54bcfab6fc62a463b35aabc71d0321320758944a
arismetyogi <arismetyogi@gmail.com>
2025-07-21 14:08:56 +0700

    feat: add docker support for user-service

--- user-service/src/main/java/com/balai/user/UserServiceApplication.java
+++ user-service/src/main/java/com/balai/user/UserServiceApplication.java
@@ -2,8 +2,10 @@ package com.balai.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity
 public class UserServiceApplication {
 
     public static void main(String[] args) {
