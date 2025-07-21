eac13e5abdc8bcb214f0f476471dbbeeccd47438
arismetyogi <arismetyogi@gmail.com>
2025-07-21 21:18:26 +0700

    fix: enableJpaAuditing to activate auditable fields

--- user-service/src/main/java/com/balai/user/UserServiceApplication.java
+++ user-service/src/main/java/com/balai/user/UserServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
@@ -13,6 +14,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 @EnableWebSecurity
 @EntityScan("com.balai.user.model.entity") // ← scan for @Entity
 @EnableJpaRepositories(basePackages = "com.balai.user.model.repository") // ← scan for your JPA repos
+@EnableJpaAuditing
 public class UserServiceApplication {
 
     public static void main(String[] args) {
