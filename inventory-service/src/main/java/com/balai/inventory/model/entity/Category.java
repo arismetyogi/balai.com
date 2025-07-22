2cda0b8461913b1d45cacdce213ecf9f14228d98
arismetyogi <arismetyogi@gmail.com>
2025-07-22 13:23:06 +0700

    fix: added relationship between category and product

--- inventory-service/src/main/java/com/balai/inventory/model/entity/Category.java
+++ inventory-service/src/main/java/com/balai/inventory/model/entity/Category.java
@@ -1,11 +1,23 @@
 package com.balai.inventory.model.entity;
 
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.util.List;
+
+@Setter
+@Getter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Category extends BaseEntity{
     private String name;
 
     @OneToMany(mappedBy = "category", cascade = CascadeType.ALL,  orphanRemoval = true)
-    private Product product;
+    private List<Product> product;
 }
--- inventory-service/src/main/java/com/balai/inventory/model/entity/Product.java
+++ inventory-service/src/main/java/com/balai/inventory/model/entity/Product.java
@@ -1,11 +1,21 @@
 package com.balai.inventory.model.entity;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 
+@Setter
+@Getter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product extends Auditable{
     private String name;
     private String description;
